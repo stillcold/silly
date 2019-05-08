627ed9360c7f5c1a78ebef6f37b7d54643885981
Stillcold <mr_kernel@163.com>
2019-05-08 23:52:24 +0800

    code: finish the entry logic

--- one/CodeMgr.lua
+++ one/CodeMgr.lua
@@ -3,6 +3,17 @@ local client = require "http.client"
 
 local CodeMgr = {}
 
+function CodeMgr:ConvertReturnToFile(httpBody, targetFilePath)
+	local file = io.open(targetFilePath, "bw+")
+	if file then
+		if file:write(httpBody) == nil then return false end
+		io.close(file)
+		return true
+	else
+		return false
+	end
+end
+
 function CodeMgr:DownLoadCode()
 	local CodeConfig = SAConfig.CodeConfig
 	for _, toDownload in pairs(CodeConfig.Alias) do
@@ -12,6 +23,7 @@ function CodeMgr:DownLoadCode()
 		print(status, head, body)
 
 		if status == 200 then
+
 			require "HttpServer"
 		end
 	end
