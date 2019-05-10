cf8edca4c6e7282eb14fb0b8c63ac25c2bfac9cc
stillcold <mr_kernel@163.com>
2019-05-10 18:07:05 +0800

    code: Make the search service more like baidu.

--- one/SearchMgr.lua
+++ one/SearchMgr.lua
@@ -1,3 +1,4 @@
+local httpIndex = require "Index"
 local searchMgr = {}
 
 local keywordTbl = require "KeywordTbl"
@@ -33,17 +34,49 @@ function searchMgr:GetAnswer(content)
 	return res
 end
 
+function searchMgr:GetDetail(content)
+	print("search text is: "..content.." lenth is "..#content)
+	local ret = {}
+	local matchCount = 0
+	for keyword,richTxt in pairs(keywordTbl) do
+		if keyword == content then
+			matchCount = matchCount + 1
+			table.insert(ret, self:GetDetailTips(matchCount))
+
+			local showTxt = self:ConvertToReadbleCode(keyword, richTxt)
+			table.insert(ret,  showTxt)
+			
+		end
+	end
+
+	local res = table.concat( ret, global.httpMultiLineTag)
+	return res
+end
+
 function searchMgr:GetSummary(resultCount)
 	if resultCount > 0 then
-		return global.summaryOkText..resultCount
+		return global.httpBoldTagBegin..global.summaryOkText..resultCount..global.httpBoldTagEnd
 	end
 
 	return global.summaryFailText
 	
 end
 
+function searchMgr:GetDetailTips(count)
+	return global.httpBoldTagBegin..global.detialOkText..count..global.httpBoldTagEnd
+end
+
 function searchMgr:ConvertToReadbleText(keyword, richTxt)
-	return global.httpBoldTagBegin..keyword..global.httpBoldTagEnd..global.httpLineEndTag..richTxt   
+	local firstWordIdx = string.find(richTxt, "%w")
+	richTxt = string.sub(richTxt, firstWordIdx)
+	richTxt = string.gsub(richTxt, "\n", "<br>")
+	richTxt = httpIndex.SearchItemContentBegin..richTxt..httpIndex.SearchItemContentEnd
+	-- return global.httpBoldTagBegin..keyword..global.httpBoldTagEnd..global.httpLineEndTag..richTxt   
+	return httpIndex.SearchItemBegin..keyword..httpIndex.SearchItemMiddle..keyword..httpIndex.SearchItemEnd..richTxt   
+end
+
+function searchMgr:ConvertToReadbleCode(keyword, richTxt)
+	return httpIndex.CodeBegin..richTxt..httpIndex.CodeEnd   
 end
 
 return searchMgr
