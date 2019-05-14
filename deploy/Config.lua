371456d5e6c68251829cba8876987d17e5fc907d
Stillcold <mr_kernel@163.com>
2019-05-14 21:43:58 +0800

    Code: add new private file for J1900.

--- deploy/Config.lua
+++ deploy/Config.lua
@@ -9,6 +9,7 @@ local config = {
 			{"encoded-codemgr-src/x_code_deploy_dir/Index.lua" , 1, true},
 			{"encoded-codemgr-src/x_code_deploy_dir/KeywordTbl.lua" , 1, true},
 			{"encoded-codemgr-src/x_code_deploy_dir/keywords/StarBiwuShowOrders.lua" , 1, true},
+			{"encoded-codemgr-src/x_code_deploy_dir/keywords/J1900.lua" , 1, true},
 		}
 	}
 	
