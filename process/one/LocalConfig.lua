3485e9139b91f94b112eca700cd62ff148f48eeb
stillcold <mr_kernel@163.com>
2019-07-30 20:04:52 +0800

    config: Change local config to default config.

--- process/one/LocalConfig.lua
+++ process/one/LocalConfig.lua
@@ -1,10 +1,10 @@
 local LocalConfig = {
 	InternalIp = "127.0.0.1",
 	InternalHttpPort = "80",
-	PublicHttpHost = "reedandwind.6655.la",
-	PublicHttpPort = "8089",
+	PublicHttpHost = "127.0.0.1",
+	PublicHttpPort = "80",
 	HttpDir = "/var/www/html/",
-	MindMapDir = "mind/SkillSet/",
+	MindMapDir = "mind/MindSet/",
 }
 
 return LocalConfig
