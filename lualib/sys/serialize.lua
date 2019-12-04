4e4e60c56002a62e0114d645fc5a944ccf364cb8
stillcold <mr_kernel@163.com>
2019-12-04 19:13:57 +0800

    framework: change serialize impl.

--- lualib/sys/serialize.lua
+++ lualib/sys/serialize.lua
@@ -2,31 +2,40 @@ local loadstring = load
 
 function serialize(obj)
     local lua = ""
+	local tbl = {}
     local t = type(obj)
     if t == "number" then
         lua = lua .. obj
+		table.insert(tbl, tostring(obj))
     elseif t == "boolean" then
         lua = lua .. tostring(obj)
+		table.insert(tbl, tostring(obj))
     elseif t == "string" then
         lua = lua .. string.format("%q", obj)
+		table.insert(tbl, string.format("%q", obj))
     elseif t == "table" then
         lua = lua .. "{\n"
-    for k, v in pairs(obj) do
-        lua = lua .. "[" .. serialize(k) .. "]=" .. serialize(v) .. ",\n"
-    end
-    local metatable = getmetatable(obj)
+		table.insert(tbl, "{\n")
+    	for k, v in pairs(obj) do
+        	lua = lua .. "[" .. serialize(k) .. "]=" .. serialize(v) .. ",\n"
+        	table.insert(tbl, "[" .. serialize(k) .. "]=" .. serialize(v) .. ",\n")
+    	end
+    	local metatable = getmetatable(obj)
         if metatable ~= nil and type(metatable.__index) == "table" then
-        for k, v in pairs(metatable.__index) do
-            lua = lua .. "[" .. serialize(k) .. "]=" .. serialize(v) .. ",\n"
-        end
-    end
+        	for k, v in pairs(metatable.__index) do
+            	lua = lua .. "[" .. serialize(k) .. "]=" .. serialize(v) .. ",\n"
+            	table.insert(tbl, "[" .. serialize(k) .. "]=" .. serialize(v) .. ",\n")
+        	end
+    	end
         lua = lua .. "}"
+		table.insert(tbl, "}")
     elseif t == "nil" then
         return nil
     else
         error("can not serialize a " .. t .. " type.")
     end
-    return lua
+	-- return lua
+	return table.concat(tbl)
 end
 
 function unserialize(lua)
