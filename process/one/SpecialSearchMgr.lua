a5ee1482da494c319ad3fe9158c134ac468eb448
stillcold <mr_kernel@163.com>
2020-01-01 21:55:37 +0800

    Search service: Add food recommend feature.

---
+++ process/one/SpecialSearchMgr.lua
@@ -0,0 +1,21 @@
+local WhereIsFood = require "WhereIsFood"
+
+local SpecialSearchMgr = {}
+
+function SpecialSearchMgr:WhereIsFood(toSearchTbl)
+	return WhereIsFood:GetFood(toSearchTbl)
+end
+
+function SpecialSearchMgr:GetSpecialResult(toSearchTbl)
+	local bMatched, specialResult
+	for k,v in ipairs(toSearchTbl) do
+		if string.find(v, "吃") then
+			bMatched = true
+			specialResult = self:WhereIsFood(toSearchTbl)
+		end
+	end
+
+	return bMatched, specialResult.."<br>"
+end
+
+return SpecialSearchMgr
