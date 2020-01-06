62eceeab92872e5483c9283e8c8b6139e82c89c1
stillcold <mr_kernel@163.com>
2020-01-07 00:27:48 +0800

    configMgr: use http server to generate the configs

--- process/configmgr/configmgr.lua
+++ process/configmgr/configmgr.lua
@@ -1,116 +1,158 @@
-local configFileMgr = require "configfilemgr"
-local localEnvConfig = require "localenvconfig"
-local configMapMgr = require "configmapmgr"
-local configMgr = {}
+local core				= require "sys.core"
 
-function configMgr:GetConfigFiles(process)
-	return configFileMgr:GetConfigFiles(process)
-end
+local localEnvConfig 	= require "localenvconfig"
+local processconfig 	= require "processconfig"
+local configMgr 		= {}
+local client			= require "http.client"
+local useDefault 		= (tonumber(core.envget("usedefault")) == 1)
+local queryConfigUrl	= core.envget("queryconfigurl")
+local showDoc			= (tonumber(core.envget("showdoc")) == 1)
 
-function configMgr:ConfigOneFile(filePath, item, value)
-	local inFile = io.open(filePath, "r")
-	local fileContent = {}
-	local matchIdx = 0
-	local idx = 0
-	if not inFile then
-		return
-	end
-
-	local lineEnd = ""
-	if string.find(filePath, "%.lua") then
-		lineEnd = ","
-	end
-
-	for line in inFile:lines() do
-		idx = idx + 1
-		table.insert(fileContent, line)
-		local pattern = item.."%s*="
-		if string.find(line, pattern) then
-			matchIdx = idx
-		end
-	end
-	inFile:close()
-
-	if matchIdx <= 0 then
-		return
-	end
-	print("\tWill config "..item.." to "..value.." in ".. filePath..", line "..matchIdx)
+function configMgr:ConfigOneFile(processName, fileConfig)
+	local filePath 	= fileConfig.path
+	local lineEnd 	= fileConfig.lineEnd
+	local fileType	= fileConfig.fileType
+	local items		= fileConfig.items
+	local lineBegin	= ""
+	
 	local outFile = io.open(filePath, "w")
-	for idx, line in ipairs(fileContent) do
-		if idx ~= matchIdx then
-			outFile:write(line.."\n")
-		else
-			if type(value) == "number" then
-				outFile:write(line:gsub("=(.+)", [[= ]]..value)..lineEnd.."\n")
-			else
-				outFile:write(line:gsub("=(.+)", [[= "]]..value..[["]])..lineEnd.."\n")
+
+	print("\tconfig "..fileType.." file "..filePath)
+	
+	if fileType == "lua" then
+		lineBegin	= "\t"
+		outFile:write([[return {]].."\n")
+	end
+
+	for idx, item in ipairs(items) do
+		local itemName		= item[1]
+		local localEnvName 	= item[2]
+		local defaultValue	= item[3]
+
+		local value	= defaultValue
+		if not useDefault and localEnvName then
+			local url = queryConfigUrl..localEnvName
+			local status, head, body = client.GET(url)
+			local httpFound = false
+			if status == 200 and body then
+				if body ~= "notfound" then
+					httpFound = true
+					value = body
+				end
+			end
+
+			if not httpFound then
+				value = localEnvConfig[localEnvName] or defaultValue
+
+				local toPrint = "\t\t Warning: server not response right for "
+				toPrint = toPrint .. localEnvName
+				toPrint = toPrint .. " --> status:"..status
+				toPrint = toPrint .. " body:"..body
+				print(toPrint)
 			end
 		end
+
+		local lineContent = itemName .. " = "
+
+		if type(value) == "number" then
+			lineContent = lineContent .. value
+		else
+			lineContent = lineContent .. [["]]..value .. [["]]
+		end
+
+		print("\t\t config "..itemName.." to "..value)
+
+		outFile:write(lineBegin..lineContent..lineEnd.."\n")
 	end
+	
+	if fileType == "lua" then
+		outFile:write([[}]])
+	end
+
 	outFile:close()
 end
 
-function configMgr:ConfigOneItemInProcess(process, item, value)
-	if not (process and item and value ) then return end
-	for vkPair in self:GetConfigFiles(process) do
-		local filePath = vkPair[1]
-		local fileType = vkPair[2]
-
-		self:ConfigOneFile(filePath, item, value)
-
+function configMgr:ConfigProcesses()
+	for processName, processConfig in pairs(processconfig) do
+		print("config process "..processName)
+		for idx1, fileConfig in pairs(processConfig) do
+			self:ConfigOneFile(processName, fileConfig)
+		end
 	end
 end
 
-function configMgr:GetLocalEnvValue(process, item)
-	-- 映射的本地配置名
-	local localEnv = configMapMgr:GetLocalEnv(process, item)
-	print("\tLocalEnv name of process " .. process .. " for item ".. item .. " is "..(localEnv or "nil"))
-	if not localEnv then return end
+function configMgr:ShowDoc()
 
-	return localEnvConfig[localEnv]
+	local rawDoc = 
+[==[
+
+Demos:
+
+QueryConfig.php:
+---
+<?php
+
+// 这就不用写注释了
+require 'XConfig.php';
+$itemName = $_GET['itemName'];
+if (!isset($itemName)){
+	echo "notfound";
+}
+$configValue = $XConfig[$itemName];
+if (!isset($configValue)){
+	echo "notfound";
+}
+echo $configValue;
+
+
+XConfig.php:
+---
+<?php
+
+$XConfig = array(
+	// 浏览器访问本地http服务的时候,需要提供的cookie,
+	// search(one) 和 todolist都是用的这个
+	'Cookie' 				=> "hi=wow1",
+	// 浏览器访问本地http服务需提供的sign
+	// search(one) 和 todolist都是用的这个
+	'Sign'					=> "sign",
+	// 展示给浏览器中的网页中的链接中的host,支持域名
+	'PublicHttpHost' 		=> "baidu.com",
+	// 展示给浏览器中的网页中的链接中的port
+	'PublicHttpPort'		=> 80,
+	// 本地根据规则生成html文件时需要找到的目录,大概率是本地网站根目录
+	'HttpDir'				=> "/var/www/html/",
+	// 脑图文件目录,会在这个目录中生成一些新的文件
+	// 也用于访问脑图
+	'MindMapDir'			=> "mindmap/",
+	// 本地Ip,大概率就是 127.0.0.1,目前多用于服务启动时候拉代码和配置信息
+	'InternalIp' 			=> "127.0.0.1",
+	// 本地部署的http端口,目前多用于服务器启动的时候拉代码和配置信息
+	'InternalHttpPort' 		=> 80,
+	// TodoList使用的数据库地址和端口
+	'TodoDbHost' 			=> "127.0.0.1:3306",
+	// TodoList使用的数据库用户
+	'TodoDbUser' 			=> "todo",
+	// TodoList使用的数据库密码
+	'TodoDbPassword' 		=> "123456",
+);
+
+]==]
+
+	local content = "Please put those two files to dir which match url:"..queryConfigUrl..rawDoc
+
+	print(content)
 end
 
-function configMgr:GetToConfigItemInProcess(process)
-	if process == "one" then
-		return {
-			"Cookie", 
-			"Sign", 
-			"InternalIp", 
-			"InternalHttpPort", 
-			"PublicHttpHost",
-			"PublicHttpPort",
-			"HttpDir",
-			"MindMapDir",
-		}
-	end
-	if process == "todolist" then
-		return {
-			"Cookie",
-			"Sign",
-			"DbUser",
-			"DbPass",
-			"DbHost",
-		}
-	end
-end
-
-function configMgr:ConfigProcess(process)
-	local toConfigItem = self:GetToConfigItemInProcess(process)
-	if not toConfigItem then return end
-
-	for _,item in ipairs(toConfigItem) do
-		print("To config item " .. item .. " in " .. process .. "...")
-		local value = self:GetLocalEnvValue(process, item)
-		self:ConfigOneItemInProcess(process, item, value)
-	end
-end
-
+-- 入口
 function configMgr:DoGen()
-	self:ConfigProcess("one")
-	self:ConfigProcess("filesync")
-	self:ConfigProcess("todolist")
-	print("done!")
-	-- self:ConfigProcessItem("one", "bootstrap", 1)
+	if showDoc then
+		self:ShowDoc()
+	else
+		self:ConfigProcesses()
+	end
+	print("done! process will quit.")
+	core.exit()
 end
 
 return configMgr
---
+++ process/configmgr/processconfig.lua
@@ -0,0 +1,58 @@
+
+return {
+	--[[
+	processName = {
+		{
+			path 	= "文件路径",
+			fileType= <config/lua>,
+			lineEnd	= 空串 或者 逗号, -- lua文件结尾有逗号,config没有
+			items	= {
+				{"条目名", 本地变量名, 默认值},
+			},
+		},
+	}
+	--]]
+
+	one = 
+	{
+		{
+			path 	= "process/one/entry.config",
+			lineEnd = "",
+			fileType="config",
+			items 	= {
+				{"daemon", nil, 0},
+				{"bootstrap", nil, "process/one/entry.lua"},
+				{"lualib_path", nil, "test/?.lua;lualib/?.lua;process/one/?.lua;"},
+				{"lualib_cpath", nil, "luaclib/?.so"},
+				{"Cookie", "Cookie", "ticket=koujue"},
+				{"Sign", "Sign", "sign"},
+			},
+		},
+		{
+			path	= "process/one/LocalConfig.lua",
+			lineEnd	= ",",
+			fileType= "lua",
+			items	= {
+				{"InternalIp", "InternalIp", "127.0.0.1"},
+				{"InternalHttpPort", "InternalHttpPort", 80},
+				{"PublicHttpHost", "PublicHttpHost", "sample.com"},
+				{"PublicHttpPort", "PublicHttpPort", 80},
+				{"HttpDir", "HttpDir", "/var/www/html/"},
+				{"MindMapDir", "MindMapDir","mindmap/"},
+
+			},
+		},
+	},
+	--[[
+	filesync = {
+		masterEntry = "process/filesync/master/entry.config",
+		masterLocal = "process/filesync/master/LocalConfig.lua",
+		slaveEntry = "process/filesync/slave/entry.config",
+		slaveLocal = "process/filesync/slave/LocalConfig.lua",
+	},
+	todolist = {
+		entry = "process/todolist/entry.config",
+	},
+	--]]
+}
+
